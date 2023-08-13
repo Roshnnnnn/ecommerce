bf178a4dde4d21b9f93a124753b9f842315be82b
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-08-14 01:07:03 +0530

    working on navbar

--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -2,28 +2,12 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectCount } from "./productListSlice";
 
-// const products = [
-// 	{
-// 		id: 1,
-// 		name: "Basic Tee",
-// 		href: "#",
-// 		imageSrc:
-// 			"https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-// 		imageAlt: "Front of men's Basic Tee in black.",
-// 		price: "$35",
-// 		color: "Black",
-// 	},
-// ];
-
-import { Fragment } from "react";
-import { Dialog, Disclousre, Menu, Transition } from "@headlessui/react";
+import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import {
-	ChevronDownIcon,
-	FunnelIcon,
-	MinusIcon,
-	PlusIcon,
-	Squares2X2Icon,
+	ChevronLeftIcon,
+	ChevronRightIcon,
+	StarIcon,
 } from "@heroicons/react/20/solid";
 
 export default function ProductList() {
